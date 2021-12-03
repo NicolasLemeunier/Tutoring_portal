056646c87e7e944e6c2e1470db2fb3f53e6db680
Nicolas Frapier--Lemeunier <21914622@etu.unicaen.fr>
2021-12-03 15:01:42 +0100

    Config mysql

---
+++ mysql_config.php
@@ -0,0 +1,7 @@
+<?php
+define("MYSQL_HOST", "mysql:host=mysql.info.unicaen.fr");
+define("MYSQL_PORT", 3306);
+define("MYSQL_DB", "21914622_bd");
+define("MYSQL_USER", "21914622");
+define("MYSQL_PASSWORD", "Iemeineeph1deime");
+?>
