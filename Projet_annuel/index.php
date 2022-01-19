80b8d917b4119b94492767b84005f473152d5b44
Nathan Grimault <21904876@etu.unicaen.fr>
2022-01-19 22:13:30 +0100

    modif index

--- Projet_annuel/index.php
+++ Projet_annuel/index.php
@@ -1,15 +1,25 @@
 <?php
 
 require_once("Router.php");
-require_once('/users/21914622/private/mysql_config.php');
+//require_once('/users/21914622/private/mysql_config.php');
+require_once('/users/21904876/private/mysql_config.php');
 require_once('storage/Storage.php');
 require_once('model/Account.php');
 require_once("view/View.php");
 require_once("view/Private_view.php");
 require_once("controller/Controller.php");
 
-$string = MYSQL_HOST . ";" . "dbname=" . MYSQL_DB . ";" . "charset=utf8";
 
-$router = new Router();
-$router->main(new Storage(new PDO($string, MYSQL_USER, MYSQL_PASSWORD)));
+try {
+
+
+  $string = MYSQL_HOST . ";" . "dbname=" . MYSQL_DB . ";" . "charset=utf8";
+
+  $router = new Router();
+  $router->main(new Storage(new PDO($string, MYSQL_USER, MYSQL_PASSWORD)));
+
+} catch (\Exception $e) {
+  echo $e;
+}
+
 ?>
