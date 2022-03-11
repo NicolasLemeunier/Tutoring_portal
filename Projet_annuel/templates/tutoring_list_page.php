162b077790441acfe8cb053964c1f3c1b4c15df8
Nathan Grimault <21904876@etu.unicaen.fr>
2022-03-11 22:07:19 +0100

    tout marche bien d'après les tests que j'ai fait, plus qu'a faire le chat des tutorats

--- Projet_annuel/templates/tutoring_list_page.php
+++ Projet_annuel/templates/tutoring_list_page.php
@@ -52,9 +52,11 @@ echo"
 
 		if(isset($_SESSION['user']) && $_SESSION['user']->getStatus() === "Tutor"){
 			if($status == "started"){
-				echo "<tr><td>Tutorat en {$key['category']}</td><td>{$status}</td><td><button><a href={$this->router->getEndTutoringURL($key['id'])}>Terminer le tutorat</a></button><button><a href={$this->router->getTutoringModificationURL($key['id'])}>Modifer</a></button><button><a href={$this->router->getTutoringDeletionURL($key['id'])}>Supprimer</a></button></td></tr>";
+				echo "<tr><td>Tutorat en {$key['category']}</td><td>{$status}</td>
+				<td><button><a href={$this->router->getEndTutoringURL($key['id'])}>Rejoindre le chat</a></button><button><a href={$this->router->getEndTutoringURL($key['id'])}>Terminer le tutorat</a></button><button><a href={$this->router->getTutoringInformationPageURL($key['id'])}>Infos</a></button><button><a href={$this->router->getTutoringModificationURL($key['id'])}>Modifer</a></button><button><a href={$this->router->getTutoringDeletionURL($key['id'])}>Supprimer</a></button></td></tr>";
 			}else if($status == "not_started" || $status == "ended"){
-				echo "<tr><td>Tutorat en {$key['category']}</td><td>{$status}</td><td><button><a href={$this->router->getStartTutoringURL($key['id'])}>Démarrer le tutorat</a></button><button><a href={$this->router->getTutoringModificationURL($key['id'])}>Modifer</a></button><button><a href={$this->router->getTutoringDeletionURL($key['id'])}>Supprimer</a></button></td></tr>";
+				echo "<tr><td>Tutorat en {$key['category']}</td><td>{$status}</td>
+				<td><button><a href={$this->router->getStartTutoringURL($key['id'])}>Démarrer le tutorat</a></button><button><a href={$this->router->getTutoringInformationPageURL($key['id'])}>Infos</a></button><button><a href={$this->router->getTutoringModificationURL($key['id'])}>Modifer</a></button><button><a href={$this->router->getTutoringDeletionURL($key['id'])}>Supprimer</a></button></td></tr>";
 			}
 		}else if(isset($_SESSION['user']) && $_SESSION['user']->getStatus() === "Student"){
 			if($status == "started"){
