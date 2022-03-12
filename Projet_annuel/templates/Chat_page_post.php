323e6293a3d9a63751beb2d7f2dbdd4d1e26f673
Nathan Grimault <21904876@etu.unicaen.fr>
2022-03-12 05:49:45 +0100

    rm fichier qui sert a rien

--- Projet_annuel/templates/Chat_page_post.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  $text = $_POST['text'];
-  $fp = fopen("log.html", 'a');
-  fwrite($fp, "<div class='msgln'>(".date("H:i:s").") <b>".$_SESSION['user']->getLogin()."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
-  fclose($fp);
-
-
- ?>
