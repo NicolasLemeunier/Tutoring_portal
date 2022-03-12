e71e43c9d3dcad1cefd85d6bbb7bcc78e08f27a0
Nathan Grimault <21904876@etu.unicaen.fr>
2022-03-12 06:37:44 +0100

    ptite modif

--- Projet_annuel/templates/Chat_page.php
+++ Projet_annuel/templates/Chat_page.php
@@ -8,13 +8,16 @@ $filename = "logs/log".$data['id'].".html"; // chaque tutorat à son fichier dif
 
 if(isset($_SESSION['user'])){
   //Simple entry message
-  if($_SESSION['user']->getStatus()=="Tutor"){
-    $txt = "Le tuteur";
-  }else{
-    $txt = "L'élève";
-  }
-  $login_message = "<div class='msgln'><span style='color:green' class='in-info'>{$txt} <b class='user-name-in'>". $_SESSION['user']->getLogin() ."</b> a rejoint le chat.</span><br></div>";
-  file_put_contents($filename, $login_message, FILE_APPEND);
+  $txt = "";
+  //if(isset($_POST['text']) && $_POST['text'] == ""){
+    if($_SESSION['user']->getStatus()=="Tutor"){
+      $txt = "Le tuteur";
+    }else if($_SESSION['user']->getStatus()=="Student"){
+      $txt = "L'élève";
+    }
+    $login_message = "<div class='msgln'><span style='color:green' class='in-info'>{$txt} <b class='user-name-in'>". $_SESSION['user']->getLogin() ."</b> a rejoint le chat.</span><br></div>";
+    file_put_contents($filename, $login_message, FILE_APPEND);
+    //}
 }
 ?>
 
@@ -44,7 +47,9 @@ if(isset($_SESSION['user'])){
 
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
 <script type="text/javascript">
- // jQuery Document
+ // jQuery Document :
+
+
  $(document).ready(function(){
    $("#exit").click(function(){
             var exit = confirm("Are you sure you want to end the session?");
@@ -62,7 +67,6 @@ if(isset($_SESSION['user'])){
 
   //Load the file containing the chat log and update the chat
   function loadLog(){
-    //Marche sauf l'auto scroll
   		var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
   		$.ajax({
   			url: "<?php echo $filename;?>",
@@ -78,8 +82,10 @@ if(isset($_SESSION['user'])){
   		  	},
   		});
   	}
-setInterval (loadLog, 1000);//1000 = 1s
+
+setInterval (loadLog, 1000);// update tous les : 1000 = 1s
  </script>
+
 
  <?php
 
@@ -93,14 +99,12 @@ setInterval (loadLog, 1000);//1000 = 1s
         }
        $logout_message = "<div class='msgln'><span style='color:red' class='left-info'>{$txt} <b class='user-name-left'>". $_SESSION['user']->getLogin() ."</b> a quitté le chat.</span><br></div>";
        file_put_contents($filename, $logout_message, FILE_APPEND | LOCK_EX);
-
        header("Location: $deco"); //Redirect the user
  }
 
 
  if(isset($_SESSION['user'])){
    if(isset($_POST['text']) && $_POST['text'] != ""){
-     var_dump($_POST);
      $text = $_POST['text'];
      $fp = fopen($filename, 'a');
      fwrite($fp, "<div class='msgln'>(".date("H:i:s").") <b>".$_SESSION['user']->getLogin()."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
